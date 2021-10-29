dee9fcd963be56c819ce10642ab0c7905498d8e3
Jon Morton <jon.morton@pm.me>
2021-10-29 14:32:31 -0500

    Execute user defined code in a web worker

---
+++ src/components/MonacoEditor.vue
@@ -0,0 +1,154 @@
+<script>
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+
+function noop() {}
+
+export { monaco };
+
+export default {
+  name: "MonacoEditor",
+  props: {
+    diffEditor: { type: Boolean, default: false },
+    width: { type: [String, Number], default: "100%" },
+    height: { type: [String, Number], default: "100%" },
+    original: String,
+    value: String,
+    language: { type: String, default: "javascript" },
+    theme: { type: String, default: "vs" },
+    options: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    editorMounted: { type: Function, default: noop },
+    editorBeforeMount: { type: Function, default: noop },
+  },
+
+  watch: {
+    options: {
+      deep: true,
+      handler(options) {
+        this.editor && this.editor.updateOptions(options);
+      },
+    },
+
+    value() {
+      this.editor &&
+        this.value !== this._getValue() &&
+        this._setValue(this.value);
+    },
+
+    language() {
+      if (!this.editor) return;
+      if (this.diffEditor) {
+        const { original, modified } = this.editor.getModel();
+        monaco.editor.setModelLanguage(original, this.language);
+        monaco.editor.setModelLanguage(modified, this.language);
+      } else
+        monaco.editor.setModelLanguage(this.editor.getModel(), this.language);
+    },
+
+    theme() {
+      this.editor && monaco.editor.setTheme(this.theme);
+    },
+
+    style() {
+      this.editor &&
+        this.$nextTick(() => {
+          this.editor.layout();
+        });
+    },
+  },
+
+  computed: {
+    style() {
+      return {
+        width: !/^\d+$/.test(this.width) ? this.width : `${this.width}px`,
+        height: !/^\d+$/.test(this.height) ? this.height : `${this.height}px`,
+      };
+    },
+  },
+
+  mounted() {
+    this.initMonaco();
+  },
+
+  beforeDestroy() {
+    this.editor && this.editor.dispose();
+  },
+
+  render(h) {
+    return <div class="monaco_editor_container" style={this.style}></div>;
+  },
+
+  methods: {
+    initMonaco() {
+      const { value, language, theme, options } = this;
+      Object.assign(options, this._editorBeforeMount());
+      this.editor = monaco.editor[
+        this.diffEditor ? "createDiffEditor" : "create"
+      ](this.$el, {
+        value: value,
+        language: language,
+        theme: theme,
+        ...options,
+      });
+      this.diffEditor && this._setModel(this.value, this.original);
+      this._editorMounted(this.editor);
+    },
+
+    _getEditor() {
+      if (!this.editor) return null;
+      return this.diffEditor ? this.editor.modifiedEditor : this.editor;
+    },
+
+    _setModel(value, original) {
+      const { language } = this;
+      const originalModel = monaco.editor.createModel(original, language);
+      const modifiedModel = monaco.editor.createModel(value, language);
+      this.editor.setModel({
+        original: originalModel,
+        modified: modifiedModel,
+      });
+    },
+
+    _setValue(value) {
+      let editor = this._getEditor();
+      if (editor) return editor.setValue(value);
+    },
+
+    _getValue() {
+      let editor = this._getEditor();
+      if (!editor) return "";
+      return editor.getValue();
+    },
+
+    _editorBeforeMount() {
+      const options = this.editorBeforeMount(monaco);
+      return options || {};
+    },
+
+    _editorMounted(editor) {
+      this.editorMounted(editor, monaco);
+      if (this.diffEditor) {
+        editor.onDidUpdateDiff((event) => {
+          const value = this._getValue();
+          this._emitChange(value, event);
+        });
+      } else {
+        console.log("not a diff editor");
+        editor.onDidChangeModelContent((event) => {
+          const value = this._getValue();
+          this._emitChange(value, event);
+        });
+      }
+    },
+
+    _emitChange(value, event) {
+      this.$emit("change", value, event);
+      this.$emit("input", value);
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,86 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <h2>Code</h2>
+      <MonacoEditor
+        theme="vs-dark"
+        language="javascript"
+        v-model="code"
+        width="500px"
+        height="280px"
+        :options="options"
+      />
+    </div>
+    <div>
+      <h2>Data</h2>
+      <MonacoEditor
+        width="500px"
+        height="280px"
+        theme="vs-dark"
+        language="json"
+        v-model="input"
+        @change="run"
+      />
+      <button @click="run">Do It.</button>
+    </div>
+    <div>
+      <h2>Output</h2>
+      {{ output }}
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import MonacoEditor from "@/components/MonacoEditor";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    MonacoEditor,
+  },
+  data() {
+    return {
+      code: `function handler (data) { return data; }`,
+      input: null,
+      output: null,
+      options: {},
+    };
+  },
+  methods: {
+    run(data) {
+      try {
+        const that = this;
+        const wrapped = `${this.code};
+          self.addEventListener('message', (e) => {
+            self.postMessage(handler.call(e, e.data));
+          }, false);`;
+        const blob = new Blob([wrapped]);
+        const url = window.URL.createObjectURL(blob);
+        const worker = new Worker(url);
+        worker.onmessage = function (e) {
+          that.output = e.data;
+          window.URL.revokeObjectURL(url);
+        };
+        worker.onerror = function (e) {
+          this.output = e;
+          window.URL.revokeObjectURL(url);
+        };
+        worker.postMessage(data);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+}
+.home > div {
+  margin-right: 1em;
+}
+.editor {
+  width: 400px;
+}
+</style>
